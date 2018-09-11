879c0c428a6214cb001047828ffe1e10f5721772
slv177 <shambazov@gmail.com>
2018-09-11 22:45:17 +0300

    .

--- EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -32,7 +32,14 @@ class EmojiArtDocumentTableViewController: UITableViewController {
 
         return cell
     }
-
+    
+    
+    
+    @IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+        emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+        tableView.reloadData()
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
